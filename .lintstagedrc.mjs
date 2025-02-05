9a6ddfd6d4e7a8e83b2441379362149ce8fbe145
dslovinsky <dslovinsky@users.noreply.github.com>
2025-02-05 16:27:39 -0500

    build: add filetypes to lint commands

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,6 +2,9 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "**/*.{json,css}": ["prettier --write --log-level silent"],
-  "*.{js,jsx,ts,tsx}": ["prettier --write --log-level silent"],
+  "*.{js,jsx,ts,tsx,mjs,mts}": [
+    "eslint --fix --quiet",
+    "prettier --write --log-level silent",
+  ],
+  "*.{json,mdx,md}": ["prettier --write --log-level silent"],
 };
