c9c7c68613c67d2ef8710e203f980ffa126bf126
Sahil Aujla <sahilaujla15@gmail.com>
2025-03-07 21:16:15 -0500

    migrate openapi specs

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,7 +5,7 @@ import {
   generateChainRpcSpec,
 } from "../src/utils/generateRpcSpecs";
 
-const schemasRoot = "src/openrpc-schemas";
+const schemasRoot = "src/openrpc";
 const outputRoot = "docs/api-specs";
 
 // generate chains OpenRPC specs
