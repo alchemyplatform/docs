1e88bb20ea9801098a40b560f136b9df05f344ff
dslovinsky <dslovinsky@users.noreply.github.com>
2025-03-14 15:25:47 -0400

    perf: optimize build command by making each part run concurrently

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-# bundle the alchemy rest api specs
+# build the alchemy rest api specs
 for file in src/openapi/*; do
   if [ -f "$file" ]; then
     filename="docs/api-specs/alchemy/rest/$(basename "$file" .yaml).json"
@@ -7,10 +7,12 @@ for file in src/openapi/*; do
       redocly lint "$filename" --format json
     ) &
   fi
-done
+done &
+
+# copy the markdown files to the docs dir
+rsync -av src/markdown docs/ &
+
+# build the rpc specs
+ts-node ./scripts/build-rpc.ts &
 
 wait
-
-cp -r src/markdown docs/markdown
-
-ts-node ./scripts/build-rpc.ts
