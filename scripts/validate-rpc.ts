d28072e285862383a56337054e7e7fdf95ea0d27
dslovinsky <dslovinsky@users.noreply.github.com>
2025-04-03 17:09:47 -0400

    build: add command to validate open-rpc specs

---
+++ scripts/validate-rpc.ts
@@ -0,0 +1,41 @@
+import { existsSync, readFileSync } from "fs";
+
+import { findFilesOfType } from "../src/utils/findFilesOfType";
+import { validateRpcSpec } from "../src/utils/validateRpcSpec";
+
+const validateOpenRpcSpecs = async (directory: string) => {
+  if (!directory) {
+    throw new Error("❌ Directory is required");
+  }
+
+  if (!existsSync(directory)) {
+    throw new Error(`❌ Directory ${directory} does not exist`);
+  }
+
+  const openRpcFiles = findFilesOfType(directory, /\.json$/);
+
+  const errors: string[] = [];
+  await Promise.all(
+    openRpcFiles.map(async (file) => {
+      try {
+        validateRpcSpec(JSON.parse(readFileSync(file, "utf8")));
+      } catch (error) {
+        if (error instanceof Error) {
+          errors.push(`❌ Error validating ${file}: ${error.message}`);
+        }
+      }
+    }),
+  );
+
+  if (errors.length > 0) {
+    throw new Error(errors.join("\n"));
+  }
+
+  const lastPartOfDirectory = directory.split("/").pop();
+  console.info(
+    `✅ Successfully validated ${lastPartOfDirectory} OpenRPC specs`,
+  );
+};
+
+validateOpenRpcSpecs("build/api-specs/alchemy/json-rpc");
+validateOpenRpcSpecs("build/api-specs/chains");
