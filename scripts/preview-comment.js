8985e22ea1cb1969e240921cd3335634f8a102f9
dslovinsky <dslovinsky@users.noreply.github.com>
2025-04-04 17:47:09 -0400

    ci: add multiple statuses to PR comment

--- scripts/preview-comment.js
+++ scripts/preview-comment.js
@@ -1,15 +1,14 @@
+// Typescript not supported in actions/github-script@v7
+// https://github.com/actions/github-script/issues/294
+const commentTitle = "🌿 Documentation Preview";
+
 /**
- * Updates or creates a comment on a GitHub pull request with a documentation preview link
- * Typescript not supported in actions/github-script@v7
- *
- * @param {object} params - The parameters object
- * @param {object} params.github - The GitHub API client instance
- * @param {object} params.context - The GitHub Actions context object
- * @param {string} params.previewUrl - The URL of the documentation preview
+ * Generates the body of a PR comment for a documentation preview
+ * @param {string} status - The status of the preview
+ * @param {string} previewUrl - The URL of the documentation preview
+ * @param {string} previousUrl - The URL of the previous successful preview build
  */
-const updatePreviewComment = async ({ github, context, previewUrl }) => {
-  const { repo, issue } = context;
-  const commentTitle = "🌿 Documentation Preview";
+const getCommentBody = (status, previewUrl, previousUrl = "") => {
   const timeUTC = new Date().toLocaleString("en-US", {
     month: "short",
     day: "numeric",
@@ -19,11 +18,53 @@ const updatePreviewComment = async ({ github, context, previewUrl }) => {
     hour12: true,
   });
 
+  const statusDisplay = {
+    success: "✅ Ready",
+    failure: "❌ Failed",
+    building: "⌛ In Progress",
+    cancelled: "⚠️ Cancelled",
+    skipped: "⏭️ Skipped",
+  };
+
+  const previousDisplay = previewUrl
+    ? `[🗃️ Previous Build](${previousUrl})`
+    : "";
+
+  const previewDisplay = {
+    success: `[🔗 Visit Preview](${previewUrl})`,
+    failure: previousDisplay,
+    building: "🏗️ Building...",
+    cancelled: previousDisplay,
+    skipped: previousDisplay,
+  };
+
+  const statusMessage = statusDisplay[status] || statusDisplay.success;
+  const previewLink = previewDisplay[status] || previewDisplay.success;
+
   const headerRow = `## ${commentTitle}\n\n`;
   const tableHeader = `| Name | Status | Preview | Updated (UTC) |\n`;
   const tableDivider = `| :--- | :------ | :------ | :------ |\n`;
-  const tableContent = `| **Alchemy Docs** | ✅ Ready | [🔗 Visit Preview](${previewUrl}) | ${timeUTC} |\n\n>`;
+  const tableContent = `| **Alchemy Docs** | ${statusMessage} | ${previewLink} | ${timeUTC} |\n\n>`;
   const commentBody = headerRow + tableHeader + tableDivider + tableContent;
+
+  return commentBody;
+};
+
+/**
+ * Updates or creates a comment on a GitHub pull request with a documentation preview link
+ * @param {object} params - The parameters object
+ * @param {object} params.github - The GitHub API client instance
+ * @param {object} params.context - The GitHub Actions context object
+ * @param {string} params.previewUrl - The URL of the documentation preview
+ * @param {string} params.status - The outcome of the preview step - `success`, `failure`, `building`, `cancelled`, or `skipped`
+ */
+const updatePreviewComment = async ({
+  github,
+  context,
+  previewUrl,
+  status = "success",
+}) => {
+  const { repo, issue } = context;
 
   const allComments = await github.rest.issues.listComments({
     owner: context.repo.owner,
@@ -36,6 +77,12 @@ const updatePreviewComment = async ({ github, context, previewUrl }) => {
   );
 
   if (existingComment) {
+    const commentBody = getCommentBody(
+      status,
+      previewUrl,
+      existingComment.body,
+    );
+
     await github.rest.issues.updateComment({
       owner: repo.owner,
       repo: repo.repo,
@@ -43,6 +90,8 @@ const updatePreviewComment = async ({ github, context, previewUrl }) => {
       body: commentBody,
     });
   } else {
+    const commentBody = getCommentBody(status, previewUrl);
+
     await github.rest.issues.createComment({
       owner: repo.owner,
       repo: repo.repo,
