d1eb5bf4ac149a663340856d5ea6a23001e80e42
dslovinsky <dslovinsky@users.noreply.github.com>
2025-05-27 11:07:37 -0400

    feat: use json-schema-ref-parser for dereferencing rpc specs

--- src/utils/generateRpcSpecs.ts
+++ src/utils/generateRpcSpecs.ts
@@ -1,10 +1,5 @@
-import defaultResolver from "@json-schema-tools/reference-resolver";
+import { dereference } from "@apidevtools/json-schema-ref-parser";
 import type { OpenrpcDocument } from "@open-rpc/meta-schema";
-import { dereferenceDocument } from "@open-rpc/schema-utils-js";
-import type { JSONSchema } from "@open-rpc/schema-utils-js/build/parse-open-rpc-document";
-import { readFileSync } from "fs";
-import yaml from "js-yaml";
-import path from "path";
 
 import {
   formatOpenRpcDoc,
@@ -74,48 +69,32 @@ export const generateAlchemyRpcSpec = async (
   filename: string,
 ) => {
   const schemaDir = `${srcDir}/${filename}`;
-  const base = getOpenRpcBase(schemaDir);
-
-  const spec: OpenrpcDocument = {
-    "x-generated-warning":
-      "⚠️ This file is auto-generated. Do not edit manually",
-    ...base,
-    "x-fern-parameters": [
-      {
-        name: "apiKey",
-        in: "path",
-        schema: {
-          type: "string",
-          default: "docs-demo",
-          description:
-            "For higher throughput, [create your own API key](https://alchemy.com/?a=docs-demo)",
-        },
-        required: true,
-      },
-    ],
-  };
-
-  // Must override default resolver
-  // dereferenceDocument second argument doesn't work because OpenRPC was coded by brain-dead monkeys
-  // @see https://github.com/open-rpc/schema-utils-js/issues/971
-  const defaultFileResolver = defaultResolver.protocolHandlerMap.file;
-  defaultResolver.protocolHandlerMap.file = async (
-    uri: string,
-    root: JSONSchema,
-  ) => {
-    if (!uri.startsWith(".")) {
-      return defaultFileResolver(uri, root);
-    }
-    // Resolve the ref path relative to the schema directory instead of root
-    const fullPath = path.resolve(schemaDir, uri);
-    const content = yaml.load(readFileSync(fullPath, "utf8")) as JSONSchema;
-    return content;
-  };
 
   try {
-    const resolvedSpec = (await dereferenceDocument(spec)) as OpenrpcDocument;
+    const spec = (await dereference(
+      `${schemaDir}/base.yaml`,
+    )) as OpenrpcDocument;
 
-    writeOpenRpcDoc(outputDir, filename, resolvedSpec);
+    const openRpcSpec = {
+      "x-generated-warning":
+        "⚠️ This file is auto-generated. Do not edit manually",
+      "x-fern-parameters": [
+        {
+          name: "apiKey",
+          in: "path",
+          schema: {
+            type: "string",
+            default: "docs-demo",
+            description:
+              "For higher throughput, [create your own API key](https://alchemy.com/?a=docs-demo)",
+          },
+          required: true,
+        },
+      ],
+      ...spec,
+    };
+
+    writeOpenRpcDoc(outputDir, filename, openRpcSpec);
   } catch (error) {
     console.error(error);
     throw new Error(`Failed to dereference ${filename}`);
